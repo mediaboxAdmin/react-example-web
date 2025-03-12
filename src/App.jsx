b91a9d7f49e6dcb852ba2a9ae4914aa5d264aef1
dukizwe <mbagapro@gmail.com>
2025-03-12 10:10:37 +0200

    test app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-import { useEffect, useRef } from 'react'
-import './App.css'
+import { useEffect, useRef } from "react"
+import "./App.css"
 
 //theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css"
 
 //core
-import "primereact/resources/primereact.min.css";
-import SideBar from './components/app/SideBar';
-import './styles/app/style.css';
+import "primereact/resources/primereact.min.css"
+import SideBar from "./components/app/SideBar"
+import "./styles/app/style.css"
 // Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 // Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import 'primeicons/primeicons.css';
-import Header from './components/app/Header';
-import RoutesProvider from './routes/RoutesProvider';
+import "bootstrap/dist/js/bootstrap.bundle.min"
+import "primeicons/primeicons.css"
+import Header from "./components/app/Header"
+import RoutesProvider from "./routes/RoutesProvider"
 import "/node_modules/primeflex/primeflex.css"
-import { Toast } from 'primereact/toast';
-import { useDispatch, useSelector } from 'react-redux';
-import { toastSelector } from './store/selectors/appSelectors';
-import { setToastAction } from './store/actions/appActions';
+import { Toast } from "primereact/toast"
+import { useDispatch, useSelector } from "react-redux"
+import { toastSelector } from "./store/selectors/appSelectors"
+import { setToastAction } from "./store/actions/appActions"
 
 function App() {
-   const toast = useRef(null);
+   const toast = useRef(null)
    const appToast = useSelector(toastSelector)
    const dispacth = useDispatch()
    useEffect(() => {
       if (appToast) {
-         toast.current.show(appToast);
+         toast.current.show(appToast)
       }
    }, [appToast])
    return (
       <div className="d-flex">
          <SideBar />
-         <Toast ref={toast} position='top-center' onHide={() => {
-            dispacth(setToastAction(null))
-         }} />
+         <Toast
+            ref={toast}
+            position="top-center"
+            onHide={() => {
+               dispacth(setToastAction(null))
+            }}
+         />
          <div className="main w-100" style={{ maxWidth: "calc(100% - 300px)" }}>
             <Header />
             <RoutesProvider />
